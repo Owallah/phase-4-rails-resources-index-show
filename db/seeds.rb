24b60ca96cd304ed66ad8b90d387f9d170d7f131
Owallah <mikesonowallah@gmail.com>
2023-06-22 12:08:33 +0300

    Completed code along

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Bird.create!(name: "Black-Capped Chickadee", species: "Poecile Atricapillus")
+# Bird.create!(name: "Grackle", species: "Quiscalus Quiscula")
+# Bird.create!(name: "Common Starling", species: "Sturnus Vulgaris")
+# Bird.create!(name: "Mourning Dove", species: "Zenaida Macroura")
+# Bird.create!(name: "Grey crowned crane", species: "Balearica regulorum")
+# Bird.create!(name: "Lilac-breasted roller", species: "Coracias caudatus")
+# Bird.create!(name: "Secretarybird", species: "Sagittarius serpentarius")
+# Bird.create!(name: "Lesser flamingo", species: "Phoeniconaias minor")
